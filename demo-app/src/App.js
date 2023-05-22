e7652b877d790657e7b780e85eea1fe606de9eba
Mirze26 <mirzaab@code.edu.az>
2023-05-23 03:52:42 +0400

    CV react

---
+++ demo-app/src/App.js
@@ -0,0 +1,86 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App d-flex">
+        <div className="container">
+          <div className="image-name">
+            <div className="image">
+              
+            </div>
+            <div className="name">
+              <h5>Beshirzade Mirze</h5>
+              <p>FullStack Developer</p>
+            </div>
+
+          </div>
+          <div className="About">
+            <p>About me:</p>
+          </div>
+          <div className="Have">
+            <p>
+            As a skilled .NET developer with diverse experience in Angular, TypeScript, JavaScript, HTML, CSS, Sass, MS SQL, and SQLite, I am driven by a passion for creating cutting-edge software solutions. With a keen eye for detail and a strong problem-solving mindset, I am constantly seeking new challenges to hone my skills and bring value to any project I work on. Throughout my career, I have demonstrated a strong commitment to delivering high-quality, reliable code that meets the needs of clients and end-users alike. Whether working independently or as part of a team, I am always eager to collaborate with others and share my knowledge and expertise.
+            </p>
+          </div>
+          <div className="Skills">
+            <ul>
+              <li>C#</li>
+              <li>ASP.NET Core</li>
+              <li>WEB API</li>
+              <li>SQL</li>
+              <li>REACT</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>JQuery</li>
+              <li>SASS</li>
+              <li>GIT</li>
+              <li>Software Architectura</li>
+              <li>Algorithms</li>
+              <li>OOP</li>
+              <li>Solid Design Principles</li>
+            </ul>
+
+          </div>
+
+
+
+        </div>
+        <div className="Education">
+          <p>Education:</p>
+          <h5>Western Caspian Collage</h5>
+          <p>September 2019</p>
+          <h5>Code Academy</h5>
+          <p>August 2022</p>
+          <div className="Language">
+            <p>Language:</p>
+            <h6>Azerbaijan-Native</h6>
+            <h6>English-Intermediate</h6>
+            <h6>Turkish-Intermediate</h6>
+          </div>
+          <div className="Contacts">
+            <p>Contacts:</p>
+            <h6>+994 50 666 41 88</h6>
+            <h6>alakbarbh@code.edu.az</h6>
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </div>
+
+
+
+  )
+  }
+
+export default App;
+
+ 
+
